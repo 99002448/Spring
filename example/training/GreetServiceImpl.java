9d3d3cf3921d7dc84eca7e54b9b5dbab00c785b0
99002448 <71481376+99002448@users.noreply.github.com>
2020-10-22 09:03:59 +0530

    Add files via upload

---
+++ example/training/GreetServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.training;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetServiceImpl implements GreetService {
+
+	public String greet(String name) {
+		
+		return "Welcome"+name;
+	}
+
+}
