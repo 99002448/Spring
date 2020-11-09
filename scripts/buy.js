c65893e894b0e55deb759f004b3da3bafcd77864
99002448 <71481376+99002448@users.noreply.github.com>
2020-11-09 17:13:10 +0530

    Add files via upload

---
+++ scripts/buy.js
@@ -0,0 +1,12 @@
+
+
+function checkFormEntry ()
+{     
+ window.location.href="BuyThankYou.html";
+	
+		
+	
+	return false;
+}
+
+document.$("form1").onsubmit=checkFormEntry;	
---
+++ scripts/thanksbuy.js
@@ -0,0 +1,15 @@
+window.alert("Your order is accepted Thanks for shopping with us!!");
+
+
+
+
+function checkFormEntry ()
+{     
+ window.location.href="ownHome.html";
+	
+		
+	
+	return false;
+}
+
+document.getElementById("form1").onsubmit=checkFormEntry;	
